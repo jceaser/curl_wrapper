5f102dbb2fec9e78feb893c17ff925ccd176979c
Thomas A Cherry <thomas.cherry@gmail.gov>
2014-08-20 11:31:57 -0400

    password support
    added account support, removed the writing of output to a file as it
    did not work

--- curl.py
+++ curl.py
@@ -22,6 +22,15 @@ class App:
         self.url_lbl = Label(frame, text="URL")
         self.url = self.lookupList('url.values', ("http://server.host.com/"))
         self.url.config(width=72)
+        
+        self.account = Frame(self.frame)
+        self.account.pack()
+        self.user_lbl = Label(self.account, text="User Name")
+        self.user = Entry(self.account)
+        self.user.config(width=8)
+        self.passwd_lbl = Label(self.account, text="Password")
+        self.passwd = Entry(self.account, text="Password", show="*")
+        self.passwd.config(width=14)
 
         self.head_lbl = Label(frame, text="HTTP Headers")
         self.head_name_lbl = Label(frame, text="Name")
@@ -58,55 +67,62 @@ Layout Phase:
 
 markdown table of the grid layout. spans between -> and <-
 
-|    | 0               | 1              | 2        | 3               | 4 | 5  |
-| -- | --------------- | -------------- | -------- | --------------- | - | -- |
-| 00 | url_l           | url -> 3       | -        | <-              |   |    |
-| 01 | head_lbl->3     | -              | <-       | headers->3 -y 7 | - | <- |
-| 02 | Head_name_lbl   | Head_name->2   | <-       | _               | - | -  |
-| 03 | head_value      | head_value->2  | <-       | _               | - | -  |
-| 04 | head_add->3     | -              | <-       | _               | - | -  |
-| 05 | param_lbl       | -              | <-       | _               | - | -  |
-| 06 | param_name_lbl  | param_name->2  | <-       | _               | - | -  |
-| 07 | param_value_lbl | param_value->2 | <-       | ^-              | - | -  |
-| 08 |                 | addFile        | addParam | remove->3       | - | <- |
-| 09 | cmd->6          | -              | -        | -               | - | <- |
-| 10 |                 |                | run      | quit            |   |    |
+|    | 0               | 1              | 2        | 3               | 4  | 5  |
+| -- | --------------- | -------------- | -------- | --------------- | -- | -- |
+| 00 | url_lbl         | url -> 3       | -        | <-              |    |    |
+| 01 | user_lbl        | user -> 2      | <-       | passwd_lbl      | pw | <- |
+| 02 | head_lbl->3     | -              | <-       | headers->3 -y 7 | -  | <- |
+| 03 | Head_name_lbl   | Head_name->2   | <-       | _               | -  | -  |
+| 04 | head_value      | head_value->2  | <-       | _               | -  | -  |
+| 05 | head_add->3     | -              | <-       | _               | -  | -  |
+| 06 | param_lbl       | -              | <-       | _               | -  | -  |
+| 07 | param_name_lbl  | param_name->2  | <-       | _               | -  | -  |
+| 08 | param_value_lbl | param_value->2 | <-       | ^-              | -  | -  |
+| 09 |                 | addFile        | addParam | remove->3       | -  | <- |
+| 10 | cmd->6          | -              | -        | -               | -  | <- |
+| 11 |                 |                | run      | quit            |    |    |
         
         '''
 
         self.url_lbl.grid(row=0,sticky=W)
         self.url.grid(row=0,column=1,columnspan=3,sticky=E)
-
-        self.head_lbl.grid(row=1,columnspan=3)
-        self.head_name_lbl.grid(row=2)
-        self.head_name.grid(row=2,column=1,columnspan=2)
-        self.head_value_lbl.grid(row=3)
-        self.head_value.grid(row=3,column=1,columnspan=2)
-        self.head_add.grid(row=4,columnspan=3)
-
-        self.param_lbl.grid(row=5,columnspan=3)
-        self.param_name_lbl.grid(row=6)
-        self.param_name.grid(row=6,column=1,columnspan=2)
-        self.param_value_lbl.grid(row=7)
-        self.param_value.grid(row=7,column=1,columnspan=2)
-        self.param_addFile.grid(row=8, column=1)
-        self.param_addParam.grid(row=8, column=2)
-
-        self.headers.grid(row=1,column=3,columnspan=2,rowspan=7)
-        self.headers_rm.grid(row=8,column=3)
         
-        self.cmd.grid(row=9,columnspan=6)
-        self.run.grid(row=10,column=2)
-        self.quit.grid(row=10,column=3)
+        self.account.grid(row=1,columnspan=6,sticky=W)
+        self.user_lbl.grid(row=0)
+        self.user.grid(row=0,column=1)
+        self.passwd_lbl.grid(row=0,column=2)
+        self.passwd.grid(row=0,column=3)
+
+        self.head_lbl.grid(row=2,columnspan=3)
+        self.head_name_lbl.grid(row=3)
+        self.head_name.grid(row=3,column=1,columnspan=2)
+        self.head_value_lbl.grid(row=4)
+        self.head_value.grid(row=4,column=1,columnspan=2)
+        self.head_add.grid(row=5,columnspan=3)
+
+        self.param_lbl.grid(row=6,columnspan=3)
+        self.param_name_lbl.grid(row=7)
+        self.param_name.grid(row=7,column=1,columnspan=2)
+        self.param_value_lbl.grid(row=8)
+        self.param_value.grid(row=8,column=1,columnspan=2)
+        self.param_addFile.grid(row=9, column=1)
+        self.param_addParam.grid(row=9, column=2)
+
+        self.headers.grid(row=2,column=3,columnspan=2,rowspan=7)
+        self.headers_rm.grid(row=9,column=3)
+        
+        self.cmd.grid(row=10,columnspan=6)
+        self.run.grid(row=11,column=2)
+        self.quit.grid(row=11,column=3)
         
         self.command = []
 
     def updateCmd(self):
         out = MutableString()
         
-        command = ["curl","-s","-X","POST","--user-agent","'Post Maker thing'"]
-        
-        template = "curl \\\n\t-s -X POST \\\n\t--user-agent 'Post Maker thing' \\\n\t%s%s"
+        command = ["curl","-s","-X","POST","--user-agent","Post_Maker_thing"]
+                
+        template = "curl \\\n\t-s -X POST \\\n\t--user-agent 'Post_Maker_thing' \\\n\t%s%s"
         list = self.headers.get(0, self.headers.size())
         for item in list:
             hh = item.find("HH: ")
@@ -114,20 +130,22 @@ markdown table of the grid layout. spans between -> and <-
             pf = item.find("PF: ")
             if hh==0:
                 parts = item[3:].split("=")
-                head = "-H '%s: %s'" % (parts[0].lstrip(), parts[1])
+                head = "-H '%s: %s'" % (trim(parts[0]), trim(parts[1]))
                 out += "%s \\\n\t" % head
-                command.append(head.rstrip().lstrip())
+                command.append("-H")
+                command.append("%s: %s" % (trim(parts[0]), trim(parts[1])) )
             elif pp==0:
                 parts = item[3:].split("=")
                 param = "--data-urlencode %s" % (item[3:])
                 out += "%s \\\n\t" % param
-                command.append(param.rstrip().lstrip())
+                command.append("--data-urlencode")
+                command.append("%s=%s" % (trim(parts[0]), trim(parts[1])) )
             elif pf==0:
                 parts = item[3:].split("=")
-                param = "--data-urlencode %s@%s" % (parts[0], parts[1])
+                param = "--data-urlencode %s@%s" % (trim(parts[0]), trim(parts[1]))
                 out += "%s \\\n\t" % param
                 command.append("--data-urlencode")
-                command.append("%s@%s" % (parts[0].lstrip(), parts[1]) )
+                command.append("%s@%s" % (trim(parts[0]), trim(parts[1])) )
 
         c = template % (out, self.url.get())
         command.append(self.url.get().rstrip())
@@ -155,11 +173,22 @@ markdown table of the grid layout. spans between -> and <-
         self.updateCmd()
 
     def doRun(self):
-        file = asksaveasfilename(title="Save output",parent=self.frame)
-        if file == "": return
-        self.command.append(">")
-        self.command.append(file)
-        print self.command
+        
+        #print self.user.get()
+        #print self.passwd.get()
+        
+        if (0<len(self.user.get()) and 0<len(self.passwd.get())):
+            self.command.insert(1, "-u")
+            self.command.insert(2, "%s:%s" % (self.user.get(), self.passwd.get()) )
+            #print "'%s:%s'" % (self.user.get(), self.passwd.get())
+
+
+        #file = asksaveasfilename(title="Save output",parent=self.frame)
+        #if file == "": return
+        #self.command.append(">")
+        #self.command.append(file)
+        
+        #print self.command
         call (self.command)
     
     def rmHeader(self):
@@ -182,8 +211,8 @@ markdown table of the grid layout. spans between -> and <-
 
     def save(self):
         urls = list(self.url['values'])
-        head_names = self.head_name['values']
-        head_values = self.head_value['values']
+        head_names = list(self.head_name['values'])
+        head_values = list(self.head_value['values'])
 
         if self.url.get() not in urls:
             urls.append(self.url.get())
@@ -193,12 +222,12 @@ markdown table of the grid layout. spans between -> and <-
         if self.head_name.get() not in head_names:
             head_names.append(self.head_name.get())
             head_names.sort()
-            self.writeLines("head.names", urls)
+            self.writeLines("head.names", head_names)
 
         if self.head_value.get() not in head_values:
             head_values.append(self.head_value.get())
             head_values.sort()
-            self.writeLines("head.values", urls)
+            self.writeLines("head.values", head_values)
 
     def writeLines(self, name, lines):
         try:
@@ -208,6 +237,11 @@ markdown table of the grid layout. spans between -> and <-
                 file.close
         except IOError:
             print ("could not write file" + name)
+
+def trim(str):
+    str = str.lstrip()
+    str = str.rstrip()
+    return str
 
 def main():
     root = Tk()
